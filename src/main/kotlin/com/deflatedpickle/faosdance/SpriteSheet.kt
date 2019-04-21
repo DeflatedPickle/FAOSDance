abdca700a99ffeeec809f08a080c2ee538c3da6b
DeflatedPickle <DeflatedPickle@gmail.com>
2019-04-21 23:30:22 +0100

    Added the spritesheet loader and window

---
+++ src/main/kotlin/com/deflatedpickle/faosdance/SpriteSheet.kt
@@ -0,0 +1,51 @@
+package com.deflatedpickle.faosdance
+
+import java.awt.Image
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+
+/**
+ * A sprite sheet
+ */
+class SpriteSheet(image: String, spriteNumX: Int, spriteNumY: Int) {
+    /**
+     * The map of sprites cut from the sheet
+     */
+    val spriteMap: MutableMap<String, MutableList<BufferedImage>> = mutableMapOf()
+
+    /**
+     * The width of the sprites
+     */
+    @Suppress("MemberVisibilityCanBePrivate")
+    var spriteWidth: Int = 0
+    /**
+     * The height of the sprites
+     */
+    @Suppress("MemberVisibilityCanBePrivate")
+    var spriteHeight: Int = 0
+
+    init {
+        val sheet = ImageIO.read(File("$image.png"))!!
+        val animations = File("$image.txt").readText()
+
+        spriteWidth = sheet.width / spriteNumX
+        spriteHeight = sheet.height / spriteNumY
+
+        var gridX = 0
+        var gridY = 0
+
+        for (anim in animations.lines()) {
+            spriteMap[anim] = mutableListOf()
+
+            for (frame in 1..spriteNumX) {
+                spriteMap[anim]!!.add(sheet.getSubimage(gridX * spriteWidth, gridY * spriteHeight, spriteWidth, spriteHeight))
+                gridX++
+            }
+
+            gridX = 0
+            gridY++
+        }
+    }
+}
---
+++ src/main/kotlin/com/deflatedpickle/faosdance/main.kt
@@ -0,0 +1,97 @@
+package com.deflatedpickle.faosdance
+
+// import org.dyn4j.dynamics.World
+// import org.dyn4j.geometry.Vector2
+import java.awt.*
+import java.awt.event.ActionListener
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.Timer
+import java.awt.Color
+import java.awt.GradientPaint
+import java.awt.AlphaComposite
+import java.awt.image.BufferedImage
+import javax.swing.JPanel
+
+
+@Suppress("KDocMissingDocumentation")
+fun main(args: Array<String>) {
+    val sheet = SpriteSheet("", 8, 10)
+    var currentAction = "Waiting"
+
+    var animFrame = 0
+
+    var reflectionPadding = 0.0
+
+    var fadeHeight = 0.65f
+    var fadeOpacity = 0.25f
+
+    var xMultiplier = 0.5
+    var yMultiplier = 0.5
+
+    val frame = JFrame("FAOSDance")
+
+    val panel = object : JPanel() {
+        init {
+            isOpaque = false
+        }
+
+        override fun paintComponent(g: Graphics) {
+            super.paintComponent(g)
+            val g2D = g as Graphics2D
+            g2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR)
+
+            g2D.translate((this.width - sheet.spriteWidth * xMultiplier) / 2, (this.height - sheet.spriteHeight * yMultiplier) / 2)
+
+            g2D.translate(0.0, -(sheet.spriteHeight * yMultiplier) / 2 - reflectionPadding)
+            g2D.scale(xMultiplier, yMultiplier)
+            g2D.drawRenderedImage(sheet.spriteMap[currentAction]!![animFrame], null)
+
+            g2D.translate(0.0, (sheet.spriteHeight * yMultiplier) * 4 + (reflectionPadding * 4))
+            g2D.scale(1.0, -1.0)
+
+            val reflection = BufferedImage(sheet.spriteWidth, sheet.spriteHeight, BufferedImage.TYPE_INT_ARGB)
+            val rg = reflection.createGraphics()
+            rg.drawRenderedImage(sheet.spriteMap[currentAction]!![animFrame], null)
+            rg.composite = AlphaComposite.getInstance(AlphaComposite.DST_IN)
+            rg.paint = GradientPaint(0f, sheet.spriteHeight.toFloat() * fadeHeight, Color(0.0f, 0.0f, 0.0f, 0.0f),
+                    0f, sheet.spriteHeight.toFloat(), Color(0.0f, 0.0f, 0.0f, fadeOpacity))
+            rg.fillRect(0, 0, sheet.spriteWidth, sheet.spriteHeight)
+            rg.dispose()
+            g2D.drawRenderedImage(reflection, null)
+        }
+    }
+    frame.add(panel)
+
+    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+
+    frame.isUndecorated = true
+    frame.isAlwaysOnTop = true
+    frame.background = Color(0, 0, 0, 0)
+
+    fun resize() {
+        val width = ((((sheet.spriteWidth * xMultiplier) * 2) * 100) / 100).toInt()
+        val height = ((((sheet.spriteHeight * yMultiplier) * 2) * 100) / 100).toInt()
+
+        frame.minimumSize = Dimension(width, height)
+        frame.setSize(width, height)
+    }
+    resize()
+
+    val timer = Timer(120, ActionListener {
+        animFrame++
+
+        if (animFrame >= 8) {
+            animFrame = 0
+        }
+
+        frame.revalidate()
+        frame.repaint()
+    })
+    timer.start()
+
+    frame.pack()
+    frame.isVisible = true
+
+    frame.setLocationRelativeTo(null)
+}
